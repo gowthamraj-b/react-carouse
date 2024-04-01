0923444184fa652541b97459ba0e22c7a21eeab6
gowthamrajlegend <gowthamrajlegend@gmail.com>
2024-04-01 09:25:15 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 1024 },
+    items: 5,
+    slidesToSlide: 2,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+export const productData = [
+  {
+    id: 1,
+    imageurl:"",
+    name: "men's shoes",
+    price: "$19.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 2,
+    imageurl:"",
+    name: "Sport shoes",
+    price: "$21.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 3,
+    imageurl:"",
+    name: "iWatch",
+    price: "$99.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 4,
+    imageurl:"",
+    name: "CMF",
+    price: "$14.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 5,
+    imageurl:"",
+    name: "Mobiile",
+    price: "$38.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 6,
+    imageurl:"",
+    name: "iPhone 12 Pro Max",
+    price: "$149.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 7,
+    imageurl:"",
+    name: "Sneakers",
+    price: "$38.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 8,
+    imageurl:"",
+    name: "Headphones",
+    price: "$49.99",
+    description: "Some text about the product..",
+  },
+];
